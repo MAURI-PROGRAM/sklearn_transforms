cfe605b5c056937cf349393839a5586ce8aff0b9
MICHAEL MERCHAN <32249510+MAURI-PROGRAM@users.noreply.github.com>
2020-09-08 13:23:13 -0500

    Se agrega clase para normalizar
    Esta clase devuelve las columnas de un df normalizadas

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,5 +1,5 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-
+from sklearn import preprocessing
 
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
@@ -14,3 +14,23 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+    
+
+
+class NormalizeData(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self.columns = columns
+
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        # Primero copiamos el dataframe de datos de entrada 'X'
+        data = X.copy()
+        data_temp = data[self.columns].values
+        min_max_scaler = preprocessing.MinMaxScaler()
+        x_scaled = min_max_scaler.fit_transform(data_temp)
+        df_temp = pd.DataFrame(x_scaled, columns=self.columns, index = data.index)
+        data[column_names_to_normalize] = df_temp
+        # Devolvemos un nuevo dataframe de datos con la data normalizada
+        return data
